dd6ac3d9a1a191c906d72e221c276daee875abe0
QuangNguyeen <nguyenvanquangngunguoi@gmail.com>
2024-12-04 16:06:27 +0700

    Update models

--- tlunews/models/User.php
+++ tlunews/models/User.php
@@ -1,1 +1,37 @@
 <?php
+class User{
+    private $id;
+    private $userName;
+    private $password;
+    private $role;  // 0 is user ; 1 is admin
+    public function __construct($id, $userName, $password, $role){
+        $this->id = $id;
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->role = $role;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getUserName(){
+        return $this->userName;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+    public function getRole(){
+        return $this->role;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function setUserName($userName){
+        $this->userName = $userName;
+    }
+    public function setPassword($password){
+        $this->password = $password;
+    }
+    public function setRole($role){
+        $this->role = $role;
+    }
+}
